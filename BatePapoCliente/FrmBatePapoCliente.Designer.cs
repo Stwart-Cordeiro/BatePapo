eb0849c4f8b2ab86573eebcce504dbe3cc7196b4
Stwart Douglas <stwart-douglas@hotmail.com>
2020-12-05 00:49:30 -0300

    Add project files.

---
+++ BatePapoCliente/FrmBatePapoCliente.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace BatePapoCliente
+{
+    partial class FrmBatePapoCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmBatePapoCliente));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtBatePapoLog = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.btnMgs = new System.Windows.Forms.Button();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Silver;
+            this.panel1.Controls.Add(this.txtBatePapoLog);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txtUser);
+            this.panel1.Controls.Add(this.txtIP);
+            this.panel1.Controls.Add(this.txtMsg);
+            this.panel1.Controls.Add(this.btnMgs);
+            this.panel1.Controls.Add(this.btnConectar);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(329, 346);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtBatePapoLog
+            // 
+            this.txtBatePapoLog.Location = new System.Drawing.Point(15, 70);
+            this.txtBatePapoLog.Multiline = true;
+            this.txtBatePapoLog.Name = "txtBatePapoLog";
+            this.txtBatePapoLog.Size = new System.Drawing.Size(304, 224);
+            this.txtBatePapoLog.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP Servidor:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Usuario:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 297);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Mensagens";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(95, 31);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(138, 23);
+            this.txtUser.TabIndex = 4;
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(95, 6);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(138, 23);
+            this.txtIP.TabIndex = 5;
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(15, 318);
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(223, 23);
+            this.txtMsg.TabIndex = 2;
+            this.txtMsg.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtMsg_KeyPress);
+            // 
+            // btnMgs
+            // 
+            this.btnMgs.Location = new System.Drawing.Point(242, 318);
+            this.btnMgs.Name = "btnMgs";
+            this.btnMgs.Size = new System.Drawing.Size(75, 23);
+            this.btnMgs.TabIndex = 1;
+            this.btnMgs.Text = "Enviar";
+            this.btnMgs.UseVisualStyleBackColor = true;
+            this.btnMgs.Click += new System.EventHandler(this.btnMgs_Click);
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(238, 31);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(81, 23);
+            this.btnConectar.TabIndex = 0;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // FrmBatePapoCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 346);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FrmBatePapoCliente";
+            this.Text = "Bate-Papo - Cliente";
+            this.Load += new System.EventHandler(this.FrmBatePapoCliente_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtBatePapoLog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.Button btnMgs;
+        private System.Windows.Forms.Button btnConectar;
+
+    }
+}
+
---
+++ BatePapoCliente/FrmBatePapoCliente.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using System.Xml;
+
+
+namespace BatePapoCliente
+{
+    public partial class FrmBatePapoCliente : Form
+    {
+
+        private string Usuario = "Desconhecido";
+        private StreamWriter swEnviado;
+        private StreamReader swReceper;
+        private TcpClient tcpServer;
+        private delegate void Atualizalogcall(string stMgs);
+        private delegate void FechaConecaocall(string stMotivo);
+        private Thread mgsThread;
+        private IPAddress endIP;
+        private bool Conecta;
+        private DataSet ds;
+        private DataTable dt;
+        private XmlDocument objxml;
+
+
+        public FrmBatePapoCliente()
+        {
+            Application.ApplicationExit += new EventHandler(OnExit);
+            InitializeComponent();
+        }
+
+        public void OnExit(object sender, EventArgs e)
+        {
+            if (Conecta == true)
+            {
+                Conecta = false;
+                swEnviado.Close();
+                swReceper.Close();
+                tcpServer.Close();
+            }
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            DataRow dr = dt.NewRow();
+            dr[0] = txtIP.Text;
+            dr[1] = txtUser.Text;
+            dr[2] = txtBatePapoLog.Text;
+            dt.Rows.Add(dr);
+            ds.WriteXml("c:/BatePapo/cliente.xml");
+
+            if (Conecta == false)
+            {
+                inicio();
+            }
+            else
+            {
+                Fecha_Conexao("Desconectado a pedido do usuário.");
+            }
+        }
+
+        private void inicio()
+        {
+            try
+            {
+                endIP = IPAddress.Parse(txtIP.Text);
+                tcpServer = new TcpClient();
+                tcpServer.Connect(endIP, 2502);
+
+                Conecta = true;
+
+                Usuario = txtUser.Text;
+
+                txtIP.Enabled = false;
+                txtUser.Enabled = false;
+                txtMsg.Enabled = true;
+                btnMgs.Enabled = true;
+                btnConectar.Text = "Desconectado";
+
+                swEnviado = new StreamWriter(tcpServer.GetStream());
+                swEnviado.WriteLine(txtUser.Text);
+                swEnviado.Flush();
+
+                mgsThread = new Thread(new ThreadStart(RecebeMgs));
+                mgsThread.Start();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Erro : " + err.Message, "Erro na conexão com servidor", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void RecebeMgs()
+        {
+            try
+            {
+                swReceper = new StreamReader(tcpServer.GetStream());
+                string ConResposta = swReceper.ReadLine();
+
+                if (ConResposta[0] == '1')
+                {
+                    this.Invoke(new Atualizalogcall(this.Atualiza), new object[] { "Conectado com sucesso!" });
+                }
+                else
+                {
+                    string Motivo = "Não Conectado: ";
+                    Motivo += ConResposta.Substring(2, ConResposta.Length - 2);
+                    this.Invoke(new FechaConecaocall(this.Fecha_Conexao), new object[] { Motivo });
+
+                    return;
+                }
+
+                while (Conecta)
+                {
+                    this.Invoke(new Atualizalogcall(this.Atualiza), new object[] { swReceper.ReadLine() });
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        private void Fecha_Conexao(string Motivo)
+        {
+            txtBatePapoLog.AppendText(Motivo + "\r\n");
+
+            txtIP.Enabled = true;
+            txtUser.Enabled = true;
+            txtMsg.Enabled = false;
+            btnMgs.Enabled = false;
+            btnConectar.Text = "Conectado";
+
+            Conecta = false;
+            swEnviado.Dispose();
+            swReceper.Dispose();
+            swEnviado.Close();
+            swReceper.Close();
+            tcpServer.Close();
+        }
+
+        private void Atualiza(string strMensagem)
+        {
+            txtBatePapoLog.AppendText(strMensagem + "\r\n");
+        }
+
+        private void EnviaMgs()
+        {
+            if (txtMsg.Lines.Length >= 1)
+            {
+                swEnviado.WriteLine(txtMsg.Text);
+                swEnviado.Flush();
+                txtMsg.Lines = null;
+            }
+            txtMsg.Text = "";
+        }
+
+        private void FrmBatePapoCliente_Load(object sender, EventArgs e)
+        {
+           ds = new DataSet();
+
+           try
+           {
+               ds.ReadXml("c:/BatePapo/cliente.xml");
+               dt = ds.Tables["Cliente"];
+           }
+           catch (FileNotFoundException ex)
+           {
+               dt = new DataTable("Cliente");
+               dt.Columns.Add("ServidorIP");
+               dt.Columns.Add("Usuario");
+               dt.Columns.Add("Msg");
+               ds.Tables.Add(dt);
+               throw ex;
+           }
+            objxml = new XmlDocument();
+            string caminho = @"c:/BatePapo/cliente.xml";
+            objxml.Load(caminho);
+
+            XmlNodeList node = objxml.SelectNodes("/NewDataSet/Cliente");
+
+            foreach (XmlNode a in node)
+            {
+                txtIP.Text = a.SelectSingleNode("ServidorIP").InnerText;
+                txtUser.Text = a.SelectSingleNode("Usuario").InnerText;
+            }
+            
+        }
+
+        private void btnMgs_Click(object sender, EventArgs e)
+        {
+            this.EnviaMgs();
+        }
+
+        private void txtMsg_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                this.EnviaMgs();
+            }
+        }
+
+      }
+}
